cf2e2021a8a425ed80a3b9b9dd3ff7db42ae6e99
Fatih Şen <fatihydrm@outlook.com>
2023-03-14 19:40:38 +0300

    edit: add new update delete get router

--- backend/pkg/routes/posts-routes.go
+++ backend/pkg/routes/posts-routes.go
@@ -8,5 +8,8 @@ import (
 
 func PostsRouter(postRouter *gin.RouterGroup) {
 	postRouter.GET("/all", middlewares.AuthMiddleware(), controllers.GetPosts)
+	postRouter.GET("/one/:id", middlewares.AuthMiddleware(), controllers.GetOnePost)
 	postRouter.POST("/create", middlewares.AuthMiddleware(), controllers.CreatePost)
+	postRouter.PUT("/update/:id", middlewares.AuthMiddleware(), controllers.UpdatePost)
+	postRouter.DELETE("/delete/:id", middlewares.AuthMiddleware(), controllers.DeletePost)
 }
