c4dec86797140c241f4864b38c7f4f041ba119e7
Fatih Şen <fatihydrm@outlook.com>
2023-03-15 20:36:15 +0300

    edit: add post and user relationship

--- backend/pkg/models/user.go
+++ backend/pkg/models/user.go
@@ -13,6 +13,7 @@ type User struct{
 	Email  		string 		 		`json:"email"`
 	Password 	string 		 		`json:"password"`
 	Token 	 	string	 		 	`json:"token"`
+	Post 	 		[]Post	 		 	`json:"posts"`
 	CreatedAt 	time.Time      	`json:"created_at"`
 	UpdatedAt 	time.Time 			`json:"updated_at"`
   	DeletedAt 	gorm.DeletedAt 	`gorm:"index" json:"deleted_at"`
