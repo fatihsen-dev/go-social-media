223e1bfbad81ad86163f02117f106edf2b32a175
Fatih Şen <fatihydrm@outlook.com>
2023-03-18 01:37:48 +0300

    edit: add cors middleware

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -6,12 +6,20 @@ import (
 
 	"github.com/fatihsen-dev/go-fullstack-social-media/pkg/routes"
 	"github.com/fatihsen-dev/go-fullstack-social-media/pkg/utils"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
+	gin.SetMode(gin.ReleaseMode)
+
 	//? create gin router
 	router := gin.Default()
+
+	//? allow cors
+	config := cors.DefaultConfig()
+   config.AllowOrigins = []string{"*"}
+   router.Use(cors.New(config))
 
 	//? Routers
 	mainRouter := router.Group("/api")
@@ -19,6 +27,10 @@ func main(){
 		routes.UsersRouter(mainRouter.Group("/user"))
 		routes.PostsRouter(mainRouter.Group("/post"))
 	}
+
+	router.GET("/api",func (c *gin.Context) {
+		c.JSON(http.StatusOK,gin.H{"message":"Api Ready"})
+	})
 	
 	//? starting server
 	log.Printf("Server started on port %s", utils.GetEnvVariable("PORT"))
