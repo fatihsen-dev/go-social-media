26b39670edf24907d77aa323727036fae09944dc
Fatih Şen <fatihydrm@outlook.com>
2023-03-18 01:40:13 +0300

    edit: add auth control routes

--- backend/pkg/routes/users-routes.go
+++ backend/pkg/routes/users-routes.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	"github.com/fatihsen-dev/go-fullstack-social-media/pkg/controllers"
+	"github.com/fatihsen-dev/go-fullstack-social-media/pkg/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func UsersRouter(usersRouter *gin.RouterGroup) {
 	usersRouter.POST("/register", controllers.Register)
 	usersRouter.POST("/login", controllers.Login)
+	usersRouter.POST("/control",middlewares.AuthMiddleware(), controllers.Control)
 }
