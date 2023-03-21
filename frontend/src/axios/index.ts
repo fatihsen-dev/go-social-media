ad16895b3fb3fc7f0664033a50cf470d7094c792
Fatih Şen <fatihydrm@outlook.com>
2023-03-21 19:54:37 +0300

    edit: add other request endpoint

--- frontend/src/axios/index.ts
+++ frontend/src/axios/index.ts
@@ -3,9 +3,72 @@ const HTTP = axios.create({
    baseURL: process.env.API_URL,
 });
 
-export const request1 = async () => {
-   return await HTTP.get("/");
-};
+export const controlRequest = async () =>
+   await HTTP.get("/user/control", {
+      headers: {
+         Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+   });
 
-export const request2 = async (data: { token: string; _id: string }) =>
-   await HTTP.post("/route2", data);
+export const registerRequest = async (user: {
+   name: string;
+   email: string;
+   password: string;
+}) => await HTTP.post("/user/register", user);
+
+export const loginRequest = async (user: { email: string; password: string }) =>
+   await HTTP.post("/user/login", user);
+
+export const postsRequest = async () =>
+   await HTTP.get("/post/all", {
+      headers: {
+         Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+   });
+
+export const createPostRequest = async (user: {
+   title: string;
+   subtitle: string;
+   description: string;
+}) =>
+   await HTTP({
+      url: "/post/create",
+      method: "POST",
+      headers: {
+         Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      data: user,
+   });
+
+export const postRequest = async (id: number) =>
+   await HTTP.get(`/post/one/${id}`, {
+      headers: {
+         Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+   });
+
+export const updatePostRequest = async (
+   user: {
+      title: string;
+      subtitle: string;
+      description: string;
+   },
+   id: number
+) =>
+   await HTTP({
+      url: `/post/update/${id}`,
+      method: "PUT",
+      headers: {
+         Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      data: user,
+   });
+
+export const deletePostRequest = async (id: number) =>
+   await HTTP({
+      url: `/post/delete/${id}`,
+      method: "DELETE",
+      headers: {
+         Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+   });
